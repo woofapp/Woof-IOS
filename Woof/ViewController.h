4a9ff116a407d7ae04c4893f9ed5f020f16b719b
niko <niko_2307@hotmail.it>
2013-02-03 22:05:52 +0100

    simpleLogin

--- Woof/ViewController.h
+++ Woof/ViewController.h
@@ -9,13 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController <UITextFieldDelegate>
-@property (weak, nonatomic) IBOutlet UIView *bannerView;
-@property (weak, nonatomic) IBOutlet UIView *loginViewContainer;
-@property (weak, nonatomic) IBOutlet UIView *formContainer;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *backgroundTaskIndicator;
-@property (weak, nonatomic) IBOutlet UILabel *labelTask;
 @property (weak, nonatomic) IBOutlet UITextField *emailText;
 @property (weak, nonatomic) IBOutlet UITextField *passwordText;
+@property (weak, nonatomic) IBOutlet UIView *formContainer;
+@property (weak, nonatomic) IBOutlet UIView *loginViewContainer;
+@property (weak, nonatomic) IBOutlet UIView *bannerView;
+@property (weak, nonatomic) IBOutlet UILabel *labelTask;
+
 
 
 - (IBAction)loginButtonClick:(id)sender;
